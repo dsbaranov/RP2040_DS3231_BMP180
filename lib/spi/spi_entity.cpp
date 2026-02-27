698390a654feed1fad8a145b43e73778edc2e12b
baranov_d <baranov_d@stilsoft.ru>
2026-02-27 17:55:35 +0300

    intermediate

--- lib/spi/spi_entity.cpp
+++ lib/spi/spi_entity.cpp
@@ -1,22 +1,43 @@
 #include "spi_entity.h"
 
-SPI::SPI(spi_inst_t *spi, uint8_t rx, uint8_t tx, uint8_t sck, uint8_t cs) : spi_(spi), cs_(cs)
+namespace spi
+{
+
+Spi::Spi(spi_inst_t *spi, uint8_t rx, uint8_t tx, uint8_t sck, uint8_t cs) : spi_(spi), cs_(cs)
 {
     init(spi, rx, tx, sck, cs);
 }
 
-void SPI::init(spi_inst_t *spi, uint8_t rx, uint8_t tx, uint8_t sck, uint8_t cs)
+void Spi::init(spi_inst_t *spi, uint8_t rx, uint8_t tx, uint8_t sck, uint8_t cs, uint32_t baudrate)
 {
     gpio_set_function(rx, GPIO_FUNC_SPI);
     gpio_set_function(tx, GPIO_FUNC_SPI);
     gpio_set_function(sck, GPIO_FUNC_SPI);
-    spi_init(spi, BAUDRATE);
+    spi_init(spi, baudrate);
     gpio_init(cs);
     gpio_set_dir(cs, GPIO_OUT);
     gpio_put(cs, 1);
 }
 
-spi_inst_t *SPI::get()
+void Spi::toggleCsPin(uint8_t cs_pin, uint8_t state)
+{
+    asm volatile("nop \n nop \n nop");
+    gpio_put(cs_pin, state);
+    asm volatile("nop \n nop \n nop");
+}
+
+void Spi::select()
+{
+    toggleCsPin(cs_, 0);
+}
+
+void Spi::deselect()
+{
+    toggleCsPin(cs_, 1);
+}
+
+const spi_inst_t *Spi::get()
 {
     return spi_;
 }
+}; // namespace spi
--- lib/spi/spi_entity.h
+++ lib/spi/spi_entity.h
@@ -1,21 +1,28 @@
 #pragma once
 #include "hardware/spi.h"
 #include "pico/stdlib.h"
-
-class SPI
+namespace spi
+{
+class Spi
 {
   public:
-    SPI(spi_inst_t *spi, uint8_t rx, uint8_t tx, uint8_t sck, uint8_t cs);
-    SPI() = delete;
-    SPI(const SPI &) = delete;
-    SPI(SPI &&) = delete;
+    static const uint32_t BAUDRATE = 1000000u;
+    Spi(spi_inst_t *spi, uint8_t rx, uint8_t tx, uint8_t sck, uint8_t cs);
+    Spi() = delete;
+    Spi(const Spi &) = delete;
+    Spi(Spi &&) = delete;
 
-    spi_inst_t *get();
+    const spi_inst_t *get();
+
+    void select();
+    void deselect();
 
   private:
-    static const uint32_t BAUDRATE = 1000000u;
     spi_inst_t *spi_;
     uint8_t cs_;
 
-    void init(spi_inst_t *spi, uint8_t rx, uint8_t tx, uint8_t sck, uint8_t cs);
+    void init(spi_inst_t *spi, uint8_t rx, uint8_t tx, uint8_t sck, uint8_t cs, uint32_t baudrate = BAUDRATE);
+
+    static void toggleCsPin(uint8_t cs_pin, uint8_t state = 0);
 };
+}; // namespace spi
